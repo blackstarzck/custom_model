fd40d00077cba4ea7da295fc7b9e9a13445db5b5
chanki <bucheongosok@gmail.com>
2024-11-19 23:32:14 +0900

    path name fixed

--- src/ex01.js
+++ src/ex01.js
@@ -43,7 +43,7 @@ export default function example() {
 	// gltf loader
 	const gltfLoader = new GLTFLoader();
 	gltfLoader.load(
-		'/models/hyundai-original.glb',
+		'./models/hyundai-original.glb',
 		gltf => {
 			console.log(gltf.scene.children[0]);
 			console.log(gltf.scene);
